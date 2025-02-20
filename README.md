b8872b802ea8b88504d27610ed1e69ac8030c0cd
bourneidBH <bourneid87@gmail.com>
2025-02-20 11:50:26 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# WordPress Divi Child Theme for Argo Industries
---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+/*================================================
+#Load the Parent theme style.css file
+================================================*/
+function dt_enqueue_styles() {
+  $parenthandle = 'divi-style'; 
+  $theme = wp_get_theme();
+  wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+    array(),  // if the parent theme code has a dependency, copy it to here
+    $theme->parent()->get('Version')
+  );
+  wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+    array( $parenthandle ),
+    $theme->get('Version') 
+  );
+}
+add_action( 'wp_enqueue_scripts', 'dt_enqueue_styles' );
